e1f35320dc01b02c5f2c2c6e36c217b492caa142
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2022-11-18 18:07:11 -0700

    Update Vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // The path on GitHub Pages. Using just the path, rather than the whole
+  // URL, in case the base URL changes.
+  base: '/group-namer/',
   plugins: [svelte()],
 })
